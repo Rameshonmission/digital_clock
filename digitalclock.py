eb174e15dac2a44a29fc0e93ddc359189d9d2b1b
Rameshonmission <thriveonmission@gmail.com>
2023-09-16 15:20:35 +0530

    added text in 2nd branch

--- digitalclock.py
+++ digitalclock.py
@@ -3,7 +3,7 @@ from tkinter import Tk, Label
 
 def time_update():
     current_time = time.strftime("%H:%M:%S %p")  # Get the current time
-    label.config(text=current_time)  # Update the label text
+    label.config(text="Welcome time")  # Update the label text
     label.after(1000, time_update)  # Update the time every 1000ms (1 second)
 
 window = Tk()

e30319f7100e678a4117b101b86515e968399a90
Rameshonmission <thriveonmission@gmail.com>
2023-09-16 15:07:44 +0530

    add additional text

--- digitalclock.py
+++ digitalclock.py
@@ -3,15 +3,15 @@ from tkinter import Tk, Label
 
 def time_update():
     current_time = time.strftime("%H:%M:%S %p")  # Get the current time
-    label.config(text=current_time)  # Update the label text
+    label.config(text="Welcome time!")  # Update the label text
     label.after(1000, time_update)  # Update the time every 1000ms (1 second)
 
 window = Tk()
 window.title("Digital Clock")
-window.geometry("600x300")  # Corrected the geometry string
+window.geometry("600x600")  # Corrected the geometry string
 
 window.configure(bg="steelblue")
-label = Label(window, text="Welcome!", font=("Arial Black", 48, "bold"), bg="steelblue")
+label = Label(window, text="Welcome time!", font=("Arial Black", 60, "bold"), bg="steelblue")
 label.pack(pady=100)
 
 # Start the time update function
